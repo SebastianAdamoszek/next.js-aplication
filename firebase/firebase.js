ae0ff2ea553e026c7b27376e4bf974c6d14b279c
SebastianAdamoszek <127986852+SebastianAdamoszek@users.noreply.github.com>
2024-09-30 14:10:31 +0200

    Update firebase.js add isSupported

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,8 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// Opcjonalnie: import Firebase Analytics, jeśli potrzebujesz
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Twoja konfiguracja Firebase z Firebase Console
 const firebaseConfig = {
@@ -19,6 +18,16 @@ const firebaseConfig = {
 // Inicjalizacja Firebase
 const app = initializeApp(firebaseConfig);
 
+// Warunkowa inicjalizacja Analytics
+isSupported().then((supported) => {
+  if (supported) {
+    const analytics = getAnalytics(app);
+    console.log("Firebase Analytics został zainicjowany.");
+  } else {
+    console.log("Firebase Analytics nie jest wspierany w tym środowisku.");
+  }
+});
+
 // Jeśli potrzebujesz Firebase Analytics, możesz go zainicjalizować
 const analytics = getAnalytics(app);  // Opcjonalnie
 
@@ -27,3 +36,15 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
+
+
+
+// const firebaseConfig = {
+//  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
