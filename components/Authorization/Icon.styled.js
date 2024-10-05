772a8293b31d145e2830ed473f107b1fbc61033c
SebastianAdamoszek <sebaadamoszek@vp.pl>
2024-10-05 18:19:02 +0200

    styled status Icon in header

--- components/Authorization/Icon.styled.js
+++ components/Authorization/Icon.styled.js
@@ -1,20 +1,22 @@
 import styled from "styled-components";
 
 export const Icon = styled.div`
-  border-radius: 50%;
-  background-color: rgba(100, 100, 100, 0.6);
-  width: 50px;
-  height: 50px;
-  // padding: 0 0 0 0;
-  font-size: 35px;
+  border: 1px solid rgba(100, 100, 100, 0.8);
+  border-radius: 10px;
+  background: linear-gradient(135deg, rgba(255, 255, 255, 0.5), rgba(100, 100, 100, 0.2));
+  width: 80px;
+  padding: 0px;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  // cursor: pointer;
+  justify-content: center;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  font-size: 35px;
   transition: 0.2s ease-in-out;
 
   &:hover {
-    transform: scale(1.03);
-    background-color: rgba(100, 100, 100, 0.8);
+    transform: scale(1.05); // Minimalnie większe skalowanie dla wyraźniejszego efektu
+    background-color: rgba(150, 150, 150, 0.6); // Zmiana koloru na ciemniejszy odcień
   }
 `;
+
