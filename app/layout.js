17f93a2179f52efa53dbb36ed9a8e35f9bee222c
SebastianAdamoszek <sebaadamoszek@vp.pl>
2024-10-06 13:20:08 +0200

    add loader

--- app/layout.js
+++ app/layout.js
@@ -1,34 +1,49 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { HeaderComponent } from "@/components/Header/Header.js";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import styles from "./layout.module.css";
 import { ChatPage } from "@/components/Chat/ChatPage.js";
+import { Loader } from "@/components/Loader/Loader";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Symulacja ładowania danych
+    const timer = setTimeout(() => {
+      setLoading(false); // Po upływie 2 sekund ustaw loading na false
+    }, 2000); // Możesz zmienić czas ładowania
+
+    return () => clearTimeout(timer); // Czyszczenie timera
+  }, []);
+
   return (
     <>
-        <html lang="en">
-          <body className={inter.className}>
-            <div className={styles.center}>
-              <Image
-                className={styles.logo}
-                src="/next.svg"
-                alt="Next.js Logo"
-                width={180}
-                height={37}
-                priority
-              />
-            </div>
-            <div>
+      <html lang="en">
+        <body className={inter.className}>
+          {loading ? (
+            <Loader />
+          ) : (
+            <div class="start-layout">
+              <div className={styles.center}>
+                <Image
+                  className={styles.logo}
+                  src="/next.svg"
+                  alt="Next.js Logo"
+                  width={180}
+                  height={37}
+                  priority
+                />
+              </div>
               <HeaderComponent />
               <ChatPage />
               <main>{children}</main>
-              
+
               <footer>
                 <ul className={styles.footer}>
                   <li>
@@ -37,8 +52,9 @@ export default function RootLayout({ children }) {
                 </ul>
               </footer>
             </div>
-          </body>
-        </html>
+          )}
+        </body>
+      </html>
     </>
   );
 }
