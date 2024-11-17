2a42b26ce32403fea6a663b45e9637854818318f
SebastianAdamoszek <sebaadamoszek@vp.pl>
2024-11-17 21:26:31 +0100

    corrected notification file firebase-messaging

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,11 +3,11 @@ importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: "AIzaSyAHTtVwXcmT2aDo9tI_sn3Xek3v2R7q_S8",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: "366009107390",
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 });
