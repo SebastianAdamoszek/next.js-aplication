578d428111ef09de8353dcd835e573bd9b0584f0
SebastianAdamoszek <sebaadamoszek@vp.pl>
2024-11-17 20:40:44 +0100

    corrected notification

--- pages/api/sendNotification.js
+++ pages/api/sendNotification.js
@@ -5,26 +5,25 @@ const serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_ADMIN_KEY);
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://addfirebasetonextjs.firebaseio.com"
+    databaseURL: "https://addfirebasetonextjs.firebaseio.com",
   });
-}
-
-export default async (req, res) => {
-  const { token, message } = req.body;
 
   const payload = {
     notification: {
-      title: message.title,
-      body: message.body,
+      title: "Nowa wiadomość",
+      body: "Tekst wiadomości",
     },
   };
 
-  try {
-    const response = await admin.messaging().sendToDevice(token, payload);
-    res.status(200).json({ success: true, response });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-};
+  const token = "USER_FCM_TOKEN"; // Token FCM użytkownika, który chcesz powiadomić
 
-
+  admin
+    .messaging()
+    .sendToDevice(token, payload)
+    .then((response) => {
+      console.log("Powiadomienie wysłane:", response);
+    })
+    .catch((error) => {
+      console.error("Błąd podczas wysyłania powiadomienia:", error);
+    });
+}
