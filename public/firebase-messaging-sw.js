f8136f2b96300fd1cc2c61b3203d496b51973ef6
SebastianAdamoszek <sebaadamoszek@vp.pl>
2024-11-17 21:37:11 +0100

    corrected notification file firebase-messaging udo keys

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,27 +1,31 @@
 //public/firebase-messaging-sw.js
-importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js");
+try {
+  importScripts("https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js");
+  importScripts("https://www.gstatic.com/firebasejs/9.6.0/firebase-messaging.js");
 
-firebase.initializeApp({
-  apiKey: "AIzaSyAHTtVwXcmT2aDo9tI_sn3Xek3v2R7q_S8",
-  authDomain: "addfirebasetonextjs.firebaseapp.com",
-  projectId: "addfirebasetonextjs",
-  storageBucket: "addfirebasetonextjs.appspot.com",
-  messagingSenderId: "366009107390",
-  appId: "1:366009107390:web:75e08da771e23347ddefef",
-  measurementId: "G-950CM3QF9L",
-});
+  firebase.initializeApp({
+    apiKey: "AIzaSyAHTtVwXcmT2aDo9tI_sn3Xek3v2R7q_S8",
+    authDomain: "addfirebasetonextjs.firebaseapp.com",
+    projectId: "addfirebasetonextjs",
+    storageBucket: "addfirebasetonextjs.appspot.com",
+    messagingSenderId: "366009107390",
+    appId: "1:366009107390:web:75e08da771e23347ddefef",
+    measurementId: "G-950CM3QF9L",
+  });
 
-const messaging = firebase.messaging();
+  const messaging = firebase.messaging();
 
-// Obsługuje powiadomienia, gdy aplikacja jest w tle lub zamknięta
-messaging.onBackgroundMessage(function (payload) {
-  console.log("Background message received ", payload);
-  const notificationTitle = "Nowa wiadomość";
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "/firebase-logo.png",
-  };
+  // Obsługuje powiadomienia, gdy aplikacja jest w tle lub zamknięta
+  messaging.onBackgroundMessage(function (payload) {
+    console.log("Background message received ", payload);
+    const notificationTitle = "Nowa wiadomość";
+    const notificationOptions = {
+      body: payload.notification.body,
+      icon: "/firebase-logo.png",
+    };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
+} catch (error) {
+  console.error("Błąd podczas inicjalizacji Firebase w serwis workerze:", error);
+}
